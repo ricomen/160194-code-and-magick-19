e1867d4aa76f86d5d63a814f2eded6d3508550ee
Timur Khamitov <t.khamitov@modulbank.ru>
2020-02-08 22:49:12 +0500

    3 итерация

--- js/stat.js
+++ js/stat.js
@@ -2,106 +2,133 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
-  var TEXT = {
-    font: '16px PT Mono',
-    padding: 20
+  var TextProps = {
+    FONT_SIZE: 16,
+    FONT_NANE: 'PT Mono',
+    LINE_HEIGHT: 20
   };
 
-  var COLOR = {
-    white: 'rgb(255,255,255)',
-    black: 'rgb(0,0,0)',
-    shadow: 'rgba(0,0,0,.7)',
-    red: 'rgba(255, 0, 0, 1)',
-    randomBlue: function () {
-      return 'hsl(240, ' + Math.floor(Math.random() * 100) + '% , 50%)';
+  var Colors = {
+    WHITE: 'rgb(255,255,255)',
+    BLACK: 'rgb(0,0,0)',
+    SHADOW: 'rgba(0,0,0,.7)',
+    RED: 'rgba(255, 0, 0, 1)',
+  };
+
+  var CloudProps = {
+    BEGIN_X: 100,
+    BEGIN_Y: 10,
+    WIDTH: 420,
+    HEIGHT: 270,
+    SHADOW_OFFSET: 10,
+    INNER_PADDING_X: 20,
+    INNER_PADDING_Y: 30,
+  };
+
+  var HistogramProps = {
+    HEIGHT: 150,
+    COLUMN_WIDTH: 40,
+    COLUMN_INTERVAL: 50,
+  };
+
+  var getRandomBlue = function () {
+    return 'hsl(240, ' + Math.floor(Math.random() * 100) + '% , 50%)';
+  };
+
+  var renderCloud = function (cloudProps, colors, textProps) {
+    var cloudTitle = ['Ура вы победили!', 'Список результатов'];
+    var CLOUD_SHADOW_BEGIN_X = cloudProps.BEGIN_X + cloudProps.SHADOW_OFFSET;
+    var CLOUD_SHADOW_BEGIN_Y = cloudProps.BEGIN_Y + cloudProps.SHADOW_OFFSET;
+    var CLOUD_BEGIN_X = cloudProps.BEGIN_X;
+    var CLOUD_BEGIN_Y = cloudProps.BEGIN_Y;
+    var CLOUD_WIDTH = cloudProps.WIDTH;
+    var CLOUD_HEIGHT = cloudProps.HEIGHT;
+    var COLOR_SHADOW = colors.SHADOW;
+    var COLOR_BLACK = colors.BLACK;
+    var COLOR_WHITE = colors.WHITE;
+    var TEXT_STYLE = textProps.FONT_SIZE + 'px ' + textProps.FONT_NANE;
+    var LINE_HEIGHT = textProps.LINE_HEIGHT;
+    var CLOUD_INNER_PADDING_X = cloudProps.INNER_PADDING_X;
+    var CLOUD_INNER_PADDING_Y = cloudProps.INNER_PADDING_Y;
+    var titleBeginX = CLOUD_BEGIN_X + CLOUD_INNER_PADDING_X;
+    var titleBeginY = CLOUD_BEGIN_Y + CLOUD_INNER_PADDING_Y;
+
+    ctx.fillStyle = COLOR_SHADOW;
+    ctx.fillRect(CLOUD_SHADOW_BEGIN_X, CLOUD_SHADOW_BEGIN_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+    ctx.strokeStyle = COLOR_BLACK;
+    ctx.strokeRect(CLOUD_BEGIN_X, CLOUD_BEGIN_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+    ctx.fillStyle = COLOR_WHITE;
+    ctx.fillRect(CLOUD_BEGIN_X, CLOUD_BEGIN_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+    ctx.fillStyle = COLOR_BLACK;
+    ctx.font = TEXT_STYLE;
+    for (var i = 0; i < cloudTitle.length; i++) {
+      if (i) {
+        titleBeginY += LINE_HEIGHT;
+      }
+      ctx.fillText(cloudTitle[i], titleBeginX, titleBeginY);
     }
   };
-
-  var CLOUD = {
-    beginX: 100,
-    beginY: 10,
-    width: 420,
-    height: 270,
-    shadowOffset: 10,
-    titleRowFirst: 'Ура вы победили!',
-    titleRowSecond: 'Список результатов',
-    innerPaddingX: 20,
-    innerPaddingY: 30,
-    render: function () {
-
-      ctx.fillStyle = COLOR.shadow;
-      ctx.fillRect(this.beginX + this.shadowOffset, this.beginY + this.shadowOffset, this.width, this.height);
-
-      ctx.strokeStyle = COLOR.black;
-      ctx.strokeRect(this.beginX, this.beginY, this.width, this.height);
-
-      ctx.fillStyle = COLOR.white;
-      ctx.fillRect(this.beginX, this.beginY, this.width, this.height);
-
-      ctx.fillStyle = COLOR.black;
-      ctx.font = TEXT.font;
-      ctx.fillText(this.titleRowFirst, this.beginX + this.innerPaddingX, this.beginY + this.innerPaddingY);
-      ctx.fillText(this.titleRowSecond, this.beginX + this.innerPaddingX, this.beginY + this.innerPaddingY + TEXT.padding);
-
-    },
+  var getMaxValue = function (arr) {
+    var max = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+    return max;
   };
 
-
-  var HISTOGRAM = {
-    height: 150,
-    columnWidth: 40,
-    columnInterval: 50,
-    columnNumber: 0,
-    names: names,
-    times: times,
-    maxTime: function () {
-      var maxTime = 0;
-      for (var i = 0; i < this.times.length; i++) {
-        if (maxTime < times[i]) {
-          maxTime = times[i];
-        }
-      }
-      return maxTime;
-    },
-
-    columnColor: function (name) {
+  var renderHistogram = function (cloudProps, histogramProps, colors, textProps) {
+    var HISTOGRAM_BEGIN_X = cloudProps.BEGIN_X + cloudProps.INNER_PADDING_X;
+    var HISTOGRAM_BEGIN_Y = cloudProps.BEGIN_Y + cloudProps.HEIGHT - cloudProps.INNER_PADDING_X;
+    var HISTOGRAM_PADDING_X = 20;
+    var HISTOGRAM_HEIGHT = histogramProps.HEIGHT;
+    var COLOR_BLACK = colors.BLACK;
+    var TEXT_STYLE = textProps.FONT_SIZE + 'px ' + textProps.FONT_NANE;
+    var MAX_TIME = getMaxValue(times);
+    var getHistogramColumnColor = function (name) {
       if (name === 'Вы') {
-        return COLOR.red;
+        return colors.RED;
       }
-      return COLOR.randomBlue();
-    },
+      return getRandomBlue();
+    };
 
-    drawColumn: function (name, time) {
-
+    var drawColumn = function (name, time, i) {
       time = Math.round(time);
-      var colTextPadding = 10;
-      var colHeight = time * this.height / this.maxTime();
-      var colBeginY = CLOUD.beginY + CLOUD.height - CLOUD.innerPaddingY - colHeight - colTextPadding;
-      var colBeginX = CLOUD.beginX + CLOUD.innerPaddingX + (this.columnWidth + this.columnInterval) * this.columnNumber;
-      var colTimeBeginY = colBeginY - colTextPadding;
-      var colNameBeginY = CLOUD.beginY + CLOUD.height - CLOUD.innerPaddingY + colTextPadding;
-      var colColor = this.columnColor(name);
-      var colTextColor = COLOR.black;
+      var COLUMN_COLOR = getHistogramColumnColor(name);
+      var COLUMN_NUMBER = i;
+      var LABEL_PADDING_Y = 10;
+      var COLUMN_WIDTH = histogramProps.COLUMN_WIDTH;
+      var COLUMN_INTERVAL = histogramProps.COLUMN_INTERVAL;
+      var COLUMN_HEIGHT = Math.round(((time * HISTOGRAM_HEIGHT) / MAX_TIME));
+      var COLUMN_BEGIN_X = HISTOGRAM_BEGIN_X + HISTOGRAM_PADDING_X + (COLUMN_WIDTH + COLUMN_INTERVAL) * COLUMN_NUMBER;
+      var COLUMN_BEGIN_Y = HISTOGRAM_BEGIN_Y;
+      var COLUMN_FILL_X = COLUMN_BEGIN_X;
+      var COLUMN_FILL_Y = HISTOGRAM_BEGIN_Y - COLUMN_HEIGHT - LABEL_PADDING_Y;
+      var LABEL_NAME_X = COLUMN_BEGIN_X;
+      var LABEL_NAME_Y = COLUMN_BEGIN_Y + LABEL_PADDING_Y;
+      var LABEL_TIME_X = COLUMN_BEGIN_X;
+      var LABEL_TIME_Y = COLUMN_FILL_Y - LABEL_PADDING_Y;
+      var LABEL_TIME = time;
+      var LABEL_NAME = name;
 
-      ctx.fillStyle = colColor;
-      ctx.fillRect(colBeginX, colBeginY, this.columnWidth, colHeight);
+      ctx.fillStyle = COLUMN_COLOR;
+      ctx.fillRect(COLUMN_FILL_X, COLUMN_FILL_Y, COLUMN_WIDTH, COLUMN_HEIGHT);
 
-      ctx.fillStyle = colTextColor;
-      ctx.fillText(time, colBeginX, colTimeBeginY);
-      ctx.fillText(name, colBeginX, colNameBeginY);
+      ctx.fillStyle = COLOR_BLACK;
+      ctx.font = TEXT_STYLE;
+      ctx.fillText(LABEL_TIME, LABEL_TIME_X, LABEL_TIME_Y);
+      ctx.fillText(LABEL_NAME, LABEL_NAME_X, LABEL_NAME_Y);
+    };
 
-      this.columnNumber++;
-
-    },
-    render: function () {
-
-      for (var i = 0; i < this.names.length; i++) {
-        this.drawColumn(names[i], times[i]);
-      }
-
+    for (var i = 0; i < names.length; i++) {
+      drawColumn(names[i], times[i], i);
     }
   };
 
-  CLOUD.render();
-  HISTOGRAM.render();
+  renderCloud(CloudProps, Colors, TextProps);
+  renderHistogram(CloudProps, HistogramProps, Colors, TextProps);
 };
